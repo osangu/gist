fb8200c42aed0e279bb2720992ff878e85b96835
osangu <osangu.dev@gmail.com>
2025-02-09 15:06:30 +0900

    [Python] :: Custom Mongo ODM v2

--- python/custom_odm/entities/abc.py
+++ python/custom_odm/entities/abc.py
@@ -1,37 +1,33 @@
-from uuid import UUID
+from typing import Annotated, Optional, ClassVar
+from pydantic import BaseModel, Field
+from pydantic import AliasChoices, PlainSerializer, PlainValidator
 
-from typing import Union, Type
-from bson.objectid import ObjectId
-
-
-class Entity(dict):
-    ID = '_id'
-    T_ID: Type[Union[ObjectId, UUID]]
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    @property
-    def id(self) -> str:
-        entity_id = self.get(self.ID)
-
-        return str(entity_id)
-
-    @id.setter
-    def id(self, value):
-        entity_id = Entity.T_ID(value)
-
-        self[self._id] = entity_id
-
-    def __getattr__(self, item):
-        """
-        value = getattr(self, item.upper())
-
-        Upper script can make circular usage
-        """
-        value = self.__getattribute__(item.upper())
-
-        return self.get(value)
+from datetime import datetime
 
 
-__all__ = ["Entity"]
+class BaseEntity(BaseModel):
+    TBL_NAME: ClassVar[str]
+
+    id: Annotated[
+        Optional[str],
+        PlainValidator(lambda x: str(x)),
+        PlainSerializer(lambda x: str(x))
+    ] = \
+        Field(
+            serialization_alias='id',
+            validation_alias=AliasChoices('id', '_id'),
+        )
+
+
+class User(BaseEntity):
+    name: str
+    email: str
+    password: str
+    is_approved: bool
+    created_at: datetime
+
+    NAME = 'name'
+    EMAIL = 'email'
+    PASSWORD = 'password'
+    CREATED_AT = 'created_at'
+    IS_APPROVED = 'is_approved'
--- python/custom_odm/entities/user.py
+++ python/custom_odm/entities/user.py
@@ -1,57 +1,28 @@
-from datetime import datetime
-from typing import Optional
+import enum
+from datetime import datetime, timedelta, timezone
 
-from bson.objectid import ObjectId
+from pydantic import Field
 
-from . import fields
-from .abc import Entity
-
-
-class User(Entity, fields.User):
-    T_ID = ObjectId
-
-    def __init__(
-            self,
-            name: str,
-            email: str,
-            password: str,
-            is_approved: bool = False,
-            _id: Optional[ObjectId] = None,
-            created_at: Optional[datetime] = None,
-    ):
-        kwargs = {
-            self.NAME: name,
-            self.EMAIL: email,
-            self.PASSWORD: password,
-            self.CREATED_AT: created_at,
-            self.IS_APPROVED: is_approved,
-        }
-        if _id is not None:
-            kwargs[self.ID] = self._id_T(_id)
-
-        if created_at is None:
-            kwargs[self.CREATED_AT] = datetime.now()
-
-        super().__init__(**kwargs)
+from . import abc
 
 
-# if __name__ == '__main__':
-#     from ..client import get_collection
-#
-#     user_col = get_collection('user')
-#
-#     user = User('osangu', 'hello@gmail.com', 'qwer1234!!')
-#
-#     res = user_col.insert_one(user)
-#     print('res', res)
-#
-#     find_user = user_col.find_one({User.EMAIL: 'hello@gmail.com'})
-#
-#     print('find_user', find_user)
-#     find_user_entity = User(**find_user)
-#
-#     print('find_user_entity', find_user_entity)
-#
-#     print('user_id', find_user_entity.id)
-#
-#     print('created_at', find_user_entity.created_at)
+def kst_now() -> datetime:
+    tz = timezone(timedelta(hours=9))
+
+    return datetime.now(tz=tz)
+
+
+class U_Role(str, enum.Enum):
+    USER = 'user'
+    ADMIN = ''
+
+
+class User(abc.User):
+    TBL_NAME = 'User'
+
+    name: str
+    email: str
+    password: str
+    is_approved: bool = Field(default=False)
+    role: U_Role = Field(default=U_Role.USER)
+    created_at: datetime = Field(default_factory=kst_now)
